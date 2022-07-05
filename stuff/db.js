c53946d84456f6478214beefceac054ed63712ba
deapthspin <thedeapthspin@gmail.com>
2022-07-05 21:52:21 +0800

    okok

--- stuff/db.js
+++ stuff/db.js
@@ -5,7 +5,10 @@ const pool = new Pool({
     password: "79a993677e48dd8e057f6a5b5b3a71a9f244a2fc7776eaefb3d0641690398ddc",
     host: "ec2-3-222-74-92.compute-1.amazonaws.com",
     port: 5432,
-    database: "d5mph160j3t2rl"
+    database: "d5mph160j3t2rl",
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 module.exports = pool
